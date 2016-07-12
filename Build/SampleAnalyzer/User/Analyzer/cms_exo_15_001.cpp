2acec4e809c186adf3e4ccfb2cc60c466df61e74
Moltialex <alexis.molter@protonmail.com>
2016-07-12 15:22:01 +0200

    some adjustements

--- Build/SampleAnalyzer/User/Analyzer/cms_exo_15_001.cpp
+++ Build/SampleAnalyzer/User/Analyzer/cms_exo_15_001.cpp
@@ -20,21 +20,24 @@ bool cms_exo_15_001::Initialize(const MA5::Configuration& cfg, const std::map<st
         INFO << "        <>  http://madanalysis.irmp.ucl.ac.be/wiki/PhysicsAnalysisDatabase" << endmsg;
         INFO << "        <><><><><><><><><><><><><><><><><><><><><><><><><>" << endmsg;
 
-        // initialize variables, histos
+        //Declare regions
         Manager()->AddRegionSelection("All");
         Manager()->AddRegionSelection("wide-jet");
         Manager()->AddRegionSelection("mass> 1.2 TeV");
 
+        //Global cut
         Manager()->AddCut("trigger");
 
+        //Cuts for "wide-jet" and "mass> 1.2 TeV" regions
         string SRForWideJetCut[]= {"wide-jet","mass> 1.2 TeV"};
         Manager()->AddCut("2+ jets", SRForWideJetCut);
-        //Manager()->AddCut("deltaR (leading jet, jet)< 1.1");
         Manager()->AddCut("delta eta (wide jet, wide jet)< 1.3", SRForWideJetCut);
 
+        //Cut only for "mass> 1.2 TeV" region
         string SRForMassCut[]= {"mass> 1.2 TeV"};
         Manager()->AddCut("mjj> 1.2 TeV", SRForMassCut);
 
+        //Declare all histograms
         Manager()->AddHisto("invariant mass", 800, 0, 8000, SRForMassCut);
         Manager()->AddHisto("pT leading jet", 350, 0, 3500, SRForMassCut);
         Manager()->AddHisto("eta leading jet", 30, -3, 3, SRForMassCut);
@@ -58,7 +61,9 @@ void cms_exo_15_001::Finalize(const SampleFormat& summary, const std::vector<Sam
 bool cms_exo_15_001::Execute(SampleFormat& sample, const EventFormat& event)
 {
 
-        if(event.rec()!=0){
+        if(event.rec()!= 0){
+
+                //Give a weight to the events
                 double myEventWeight;
                 if(Configuration().IsNoEventWeight()) myEventWeight=1.;
                 else if(event.mc()->weight()!=0.) myEventWeight=event.mc()->weight();
@@ -76,21 +81,21 @@ bool cms_exo_15_001::Execute(SampleFormat& sample, const EventFormat& event)
                 TLorentzVector widejettwo;
 
                 //Declare transverse hadronic energy and pass criteria
-                int good= 0;
+                bool good= false;
                 double HT= 0;
 
                 //Compute transverse hadronic energy and look if the event have a jet with pT> 500
-                for(unsigned int j= 0; j<event.rec()->jets().size(); j++){
+                for(unsigned int j= 0; j< event.rec()->jets().size(); j++){
                         const RecJetFormat *CurrentJet= &(event.rec()->jets()[j]);
                         double pt= CurrentJet->momentum().Pt();
                         double eta= CurrentJet->momentum().Eta();
                         if(pt> 40 && fabs(eta)< 3) HT+= pt;
-                        if(pt> 500) good++;
+                        if(pt> 500) good= true;
                 }
 
-                //Put jet with pT> 30 and eta< 2.5 inside a vector
-                if(Manager()->ApplyCut(HT> 800 || good>= 1, "trigger")){
-                        for(unsigned int j= 0; j<event.rec()->jets().size(); j++){
+                //Put jet with pT> 30 and eta< 2.5 inside the container 
+                if(Manager()->ApplyCut(HT> 800 || good, "trigger")){
+                        for(unsigned int j= 0; j< event.rec()->jets().size(); j++){
                                 const RecJetFormat *CurrentJet= &(event.rec()->jets()[j]);
                                 double pt= CurrentJet->momentum().Pt();
                                 double eta= CurrentJet->momentum().Eta();
@@ -102,12 +107,13 @@ bool cms_exo_15_001::Execute(SampleFormat& sample, const EventFormat& event)
                 //Sort jet's container
                 SORTER->sort(myJets);
 
+                //Check number of jets inside the container
                 if(Manager()->ApplyCut(myJets.size()>1, "2+ jets")){
                         //Leading jet become wide-jet
                         widejetone= myJets[0]->momentum();
                         widejettwo= myJets[1]->momentum();
 
-                        //Nearest jet will addition with wide-jet
+                        //Nearest jets will addition with wide-jet
                         for(unsigned int j= 2; j<myJets.size(); j++){
                                 double deltaR1= myJets[0]->dr(myJets[j]);
                                 double deltaR2= myJets[1]->dr(myJets[j]);
@@ -124,20 +130,25 @@ bool cms_exo_15_001::Execute(SampleFormat& sample, const EventFormat& event)
                         }
                 }
 
+                //Keep the wide-jet with delta eta< 1.3
                 if(!Manager()->ApplyCut(fabs(widejetone.Eta()-widejettwo.Eta())< 1.3, "delta eta (wide jet, wide jet)< 1.3")){
                         return true;
                 }
 
+                //Create a TLorentzVector with the momentum of the two wide-jets
                 TLorentzVector mom= widejetone+widejettwo;
+                //Compute the invariant mass
                 double mass= mom.M();
 
+                //Keep the invariant mass> 1.2 TeV
                 if(!Manager()->ApplyCut(mass> 1200, "mjj> 1.2 TeV")){
                         return true;
                 }
-                
-                        Manager()->FillHisto("invariante mass", mass);
-                        Manager()->FillHisto("pT leading jet", mom.Pt());
-                        Manager()->FillHisto("eta leading jet", mom.Eta());
+
+                //Fill the histograms
+                Manager()->FillHisto("invariante mass", mass);
+                Manager()->FillHisto("pT leading jet", mom.Pt());
+                Manager()->FillHisto("eta leading jet", mom.Eta());
 
         }
 
